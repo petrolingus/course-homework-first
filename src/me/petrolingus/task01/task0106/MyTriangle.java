19be278cea7023413087e9eec410978ad7de1786
petrolingus <petrolingus@gmail.com>
2021-03-23 02:49:14 +0300

    changes in getType method of MyTriangle class

--- src/me/petrolingus/task01/task0106/MyTriangle.java
+++ src/me/petrolingus/task01/task0106/MyTriangle.java
@@ -2,11 +2,11 @@ package me.petrolingus.task01.task0106;
 
 public class MyTriangle {
 
-    private MyDoublePoint v1;
+    private final MyDoublePoint v1;
 
-    private MyDoublePoint v2;
+    private final MyDoublePoint v2;
 
-    private MyDoublePoint v3;
+    private final MyDoublePoint v3;
 
     public MyTriangle(double x1, double y1, double x2, double y2, double x3, double y3) {
         this(new MyDoublePoint(x1, y1), new MyDoublePoint(x2, y2), new MyDoublePoint(x3, y3));
@@ -35,12 +35,18 @@ public class MyTriangle {
     }
 
     public String getType() {
+
         double side1 = v1.distance(v2);
         double side2 = v2.distance(v3);
         double side3 = v3.distance(v1);
-        if (side1 == side2 && side1 == side3) {
+
+        boolean comp12 = Double.compare(side1, side2) == 0;
+        boolean comp23 = Double.compare(side2, side3) == 0;
+        boolean comp31 = Double.compare(side3, side1) == 0;
+
+        if (comp12 && comp23) {
             return "Equilateral";
-        } else if (side1 == side2 || side2 == side3 || side3 == side1) {
+        } else if (comp12 || comp23 || comp31) {
             return "Isosceles";
         } else {
             return "Scalene";
