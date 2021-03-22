e5be8ca0c1ed7bbbc77e7b4b20ceb32aeaeedf2c
petrolingus <petrolingus@gmail.com>
2021-03-23 02:42:45 +0300

    change type of vertices in MyTriangle from MyPoint to MyDoublePoint

--- src/me/petrolingus/task01/task0106/MyTriangle.java
+++ src/me/petrolingus/task01/task0106/MyTriangle.java
@@ -1,20 +1,18 @@
 package me.petrolingus.task01.task0106;
 
-import me.petrolingus.task01.task0105.MyPoint;
-
 public class MyTriangle {
 
-    private MyPoint v1;
+    private MyDoublePoint v1;
 
-    private MyPoint v2;
+    private MyDoublePoint v2;
 
-    private MyPoint v3;
+    private MyDoublePoint v3;
 
-    public MyTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
-        this(new MyPoint(x1, y1), new MyPoint(x2, y2), new MyPoint(x3, y3));
+    public MyTriangle(double x1, double y1, double x2, double y2, double x3, double y3) {
+        this(new MyDoublePoint(x1, y1), new MyDoublePoint(x2, y2), new MyDoublePoint(x3, y3));
     }
 
-    public MyTriangle(MyPoint v1, MyPoint v2, MyPoint v3) {
+    public MyTriangle(MyDoublePoint v1, MyDoublePoint v2, MyDoublePoint v3) {
         this.v1 = v1;
         this.v2 = v2;
         this.v3 = v3;
