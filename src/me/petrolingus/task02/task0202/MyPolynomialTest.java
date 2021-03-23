fedce958457d718dabecc0bf0edb7f19b7ee28ac
petrolingus <petrolingus@gmail.com>
2021-03-23 09:54:27 +0300

    changes in MyPolynomialTest class

--- src/me/petrolingus/task02/task0202/MyPolynomialTest.java
+++ src/me/petrolingus/task02/task0202/MyPolynomialTest.java
@@ -4,9 +4,26 @@ public class MyPolynomialTest {
 
     public static void main(String[] args) {
 
-        MyPolynomial polynomial = new MyPolynomial(2, 1, 1);
+        MyPolynomial polynomial = new MyPolynomial(2, 3, 1);
 
-        System.out.println(polynomial);
+        double x = 0;
+        double y = polynomial.evaluate(x);
+        System.out.println("f(x) = " + polynomial);
+        System.out.println("f(" + x + ") = " + y + "\n");
+
+        MyPolynomial polynomial2 = new MyPolynomial(2, 5, 1);
+        MyPolynomial polynomial3 = new MyPolynomial(1, 4, 6, 1);
+        System.out.println("g(x) = " + polynomial2);
+        System.out.println("h(x) = " + polynomial3);
+        MyPolynomial sum = polynomial2.add(polynomial3);
+        System.out.println("g(x) + h(x) = " + sum);
+
+        polynomial2 = new MyPolynomial(2, 1);
+        polynomial3 = new MyPolynomial(4, 5, 6);
+        System.out.println("g(x) = " + polynomial2);
+        System.out.println("h(x) = " + polynomial3);
+        MyPolynomial mul = polynomial2.add(polynomial3);
+        System.out.println("g(x) * h(x) = " + mul);
 
     }
 }
