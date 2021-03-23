39a0dcbcb9cb59e07affed38c669263ea9866667
petrolingus <petrolingus@gmail.com>
2021-03-23 09:26:23 +0300

    changes in MyComplexTest class

--- src/me/petrolingus/task02/task0201/MyComplexTest.java
+++ src/me/petrolingus/task02/task0201/MyComplexTest.java
@@ -10,5 +10,11 @@ public class MyComplexTest {
         MyComplex complex1 = new MyComplex(1, 1);
         System.out.println(complex1);
         System.out.println("complex1 arg = " + complex1.argument());
+
+        complex0.setValue(6, 2);
+        System.out.println("Set complex0 to " + complex0);
+        System.out.print("complex0.multiply(complex1) => ");
+        complex0.multiply(complex1);
+        System.out.println(complex0);
     }
 }
